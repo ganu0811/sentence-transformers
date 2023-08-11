e125316842bc33b799c1816e1d2d958d4774b124
ganesh <ganesh.adarkar0811@gmail.com>
2023-08-11 10:12:50 +0100

    Modified changes

--- sentence_transformers/losses/gBCELoss.py
+++ sentence_transformers/losses/gBCELoss.py
@@ -5,11 +5,20 @@ import torch
 
 # Define the Generalised Binary Cross Entropy (gBCE) loss
 def gBCE(predicted_scores, true_probabilities, beta):
+     
+     
+     print(predicted_scores)
+     print(true_probabilities)
      predicted_probs=torch.sigmoid(predicted_scores)
+     eps= 1e-5
+     #predicted_probs=torch.clamp(predicted_probs,min=eps, max=1-eps)
      positive_part=true_probabilities*(torch.log(torch.pow(predicted_probs,beta)))
      negative_part=(1-true_probabilities)*(torch.log(1-predicted_probs))
      # sigmoid_beta = sigmoid(beta * scores[0])  # for the positive example
      # neg_scores = [sigmoid(scores[i]) for i in negative_indices]  # for the negative examples
+     print(predicted_probs)
+     print(positive_part)
+     print(negative_part)
      return - (positive_part + negative_part)
 
 
